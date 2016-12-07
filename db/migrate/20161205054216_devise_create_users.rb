8b5b4658e9f1254c96ae100a633db118ad4437f1
SHOJI  KAZUMA : ) <shoji0412@ezweb.ne.jp>
2016-12-07 15:33:03 +0900

    usersテーブル編集

--- db/migrate/20161205054216_devise_create_users.rb
+++ db/migrate/20161205054216_devise_create_users.rb
@@ -5,6 +5,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.text :avatar
+      t.text :profile
+      t.string :member
+      t.text :works
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
