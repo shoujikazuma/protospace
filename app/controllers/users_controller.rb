203a5896bd897cf697a8b86401043bb2a2b90ccd
SHOJI  KAZUMA : ) <shoji0412@ezweb.ne.jp>
2017-01-05 20:11:08 +0900

    userのshowページ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,14 @@ class UsersController < ApplicationController
   end
 
   def show
+    user = User.find(params[:id])
+    @email = user.email
+    @id = user.id
+    @avatar = user.avatar
+    @profile = user.profile
+    @member = user.member
+    @work =user.works
+    @nickname = user.nickname
   end
   
 end
